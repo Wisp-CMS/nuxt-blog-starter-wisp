5763c0168a58b2d9946c1b0d9001452cb449774f
Raymond Yeh <ray@geek.sg>
2024-07-24 22:07:26 +0800

    feat: simple header

---
+++ components/HeaderBar.vue
@@ -0,0 +1,75 @@
+<template>
+  <header class="bg-white">
+    <nav
+      class="mx-auto flex items-center justify-between mt-4 mb-4"
+      aria-label="Global"
+    >
+      <a href="/"><div class="font-bold text-3xl">Blog</div></a>
+      <div class="flex lg:hidden">
+        <button
+          type="button"
+          class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+          @click="mobileMenuOpen = true"
+        >
+          <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+        </button>
+      </div>
+      <div class="hidden lg:flex lg:gap-x-12">
+        <a
+          v-for="item in navigation"
+          :key="item.name"
+          :href="item.href"
+          class="text-sm font-semibold leading-6 text-gray-900"
+          >{{ item.name }}</a
+        >
+      </div>
+    </nav>
+    <Dialog
+      class="lg:hidden"
+      :open="mobileMenuOpen"
+      @close="mobileMenuOpen = false"
+    >
+      <div class="fixed inset-0 z-10" />
+      <DialogPanel
+        class="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10"
+      >
+        <div class="flex items-center justify-between">
+          <div />
+          <button
+            type="button"
+            class="-m-2.5 rounded-md p-2.5 text-gray-700"
+            @click="mobileMenuOpen = false"
+          >
+            <XMarkIcon class="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+        <div class="mt-6 flow-root">
+          <div class="-my-6 divide-y divide-gray-500/10">
+            <div class="space-y-2 py-6">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >{{ item.name }}</a
+              >
+            </div>
+          </div>
+        </div>
+      </DialogPanel>
+    </Dialog>
+  </header>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { Dialog, DialogPanel } from "@headlessui/vue";
+import { Bars3Icon, XMarkIcon } from "@heroicons/vue/24/outline";
+
+const navigation = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+];
+
+const mobileMenuOpen = ref(false);
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="max-w-4xl mx-auto p-4">
+    <HeaderBar />
+    <slot />
+  </div>
+</template>
